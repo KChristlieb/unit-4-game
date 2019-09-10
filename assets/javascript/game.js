8efa8104d8fba769097d4501076d9ced61f0804c
Kade Christlieb <kchristlieb@gmail.com>
2019-09-10 16:00:22 -0600

    added a google font and for testing added text and images to the html for spacing and layout. Coded glbal arryas in JS and image arrays in JS.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function){
+
+    // Global Variables
+
+    var wins = 0;
+    var losses = 0;
+    $('.wins').text('Wins ' = wins);
+    $('losses').text('Losses ' + losses);
+
+    // array for images
+    var crystalsImages = ["assets/images/blue.png", "assets/images/green.png", "assets/images/purple.png", "assets/images/yellow.png"]
+}
