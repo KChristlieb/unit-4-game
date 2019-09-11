ca0df4d1f43ceb875fabc6162ae87d442b6e98f0
Kade Christlieb <kchristlieb@gmail.com>
2019-09-11 16:23:10 -0600

    cleaned up code and made spacing in code better for readablity. Still having error when game is ran and finished it will duplicate the crystals instead of just keeping the image that is there.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Global Variables
-
     var wins = 0;
     var losses = 0;
     $(".wins").text("Wins: " + wins);
@@ -12,29 +11,70 @@ $(document).ready(function() {
 
     // generating random nums for crystals
     function crystalsValues() {
+
         for (var i = 0; i < crystalsImages.length; i++) {
+
             var images = $("<img>");
-            images.addClass("crystal-buttons crystal crystal-images");
+            images.addClass("crystal-buttons crystal crystal-image");
             images.attr("src", crystalsImages[i]);
-            images.attr("date-letter", Math.floor(Math.random() *12) *+1);
+            images.attr("data-letter", Math.floor(Math.random() * 12) * +1);
             $("#crystals").append(images);
-        };
-    };
+        }
+
+    }
 
     function playGame() {
+
 
         var counter = 0;
         $(".score").text("Your Score: " + counter);
 
         // generates random target number for player to reach
-        var targetNum = Math.floor(Math.random() * (120-19) + 19);
+        var targetNum = Math.floor(Math.random() * (120 - 19) + 19);
 
         // displays target number on the dom
         $(".numguess").text("Number to reach :" + targetNum);
         console.log(targetNum);
+
+        // on click event for when user clicks a crystal
+        $(".crystal-buttons").click(function () {
+
+            // generating a number for each crystal when it is clicked
+
+            crystalIsClicked = true;
+            var crystalsValue = ($(this).attr("data-letter"));
+            crystalsValue = parseInt(crystalsValue);
+            // adds the click value to the score counter
+            counter += crystalsValue;
+
+            console.log(crystalsValue);
+            console.log(counter);
+
+            $(".score").text("Your Score: " + counter);
+
+            if (counter === targetNum) {
+            alert("You Win!!!");
+            wins++;
+            $(".wins").text("Wins: " + wins);
+            $("#crytals").empty();
+            crystalsValues();
+            playGame();
+            }
+
+            else if (counter >= targetNum) {
+            alert("Sorry, you lost :(");
+            losses++;
+            $(".losses").text("Losses: " + losses);
+            $("#crytals").empty();
+            crystalsValues();
+            playGame();
+            }
+
+        });
     }
 
 
     crystalsValues();
     playGame();
+
 });
